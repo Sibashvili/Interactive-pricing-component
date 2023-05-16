32d181a6f1537581449eed3719ff6dd08237a7f3
Giorgi Sibashvili <124514809+Sib063@users.noreply.github.com>
2023-05-16 17:54:13 +0400

    changed javascript function and slider styles

--- script.js
+++ script.js
@@ -13,25 +13,23 @@ console.log(slideAfter);
 const slider = document.querySelector(".slider");
 
 function myChange() {
-  console.log(checkBox.checked);
-
-  if (slider.value === "1") {
+  if (slider.value === "1" && !checkBox.checked) {
     slideAfter.style.width = "0";
     money.innerHTML = "$8.00";
     view.innerHTML = "10K PAGEWIEVS";
-  } else if (slider.value === "2") {
+  } else if (slider.value === "2" && !checkBox.checked) {
     slideAfter.style.width = "25%";
     money.innerHTML = "$12.00";
     view.innerHTML = "50K PAGEWIEVS";
-  } else if (slider.value === "3") {
+  } else if (slider.value === "3" && !checkBox.checked) {
     slideAfter.style.width = "50%";
     money.innerHTML = "$16.00";
     view.innerHTML = "100K PAGEWIEVS";
-  } else if (slider.value === "4") {
+  } else if (slider.value === "4" && !checkBox.checked) {
     slideAfter.style.width = "75%";
     money.innerHTML = "$24.00";
     view.innerHTML = "500K PAGEWIEVS";
-  } else if (slider.value === "5") {
+  } else if (slider.value === "5" && !checkBox.checked) {
     slideAfter.style.width = "100%";
     money.innerHTML = "$36.00";
     view.innerHTML = "1M PAGEWIEVS";
